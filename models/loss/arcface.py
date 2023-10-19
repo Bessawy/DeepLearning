11d8a498858c9e3740e579610c075d8f679b464f
Amr Alkhashab <amr.alkhashab@innokasmedical.fi>
2023-10-19 14:01:44 +0300

    Add: ArcFace

--- models/loss/arcface.py
+++ models/loss/arcface.py
@@ -4,6 +4,11 @@ import torch.nn.functional as F
 
 class ArcFace(nn.Module):
     def __init__(self, feature_in, feature_out=24, margin=0.3, scale = 64):
+        '''
+            params:
+                feature_in: embed sized
+                feature_out: number of classes
+        '''
         super().__init__()
         self.feature_in = feature_in
         self.feature_out = feature_out
@@ -12,6 +17,7 @@ class ArcFace(nn.Module):
         
         self.weights = nn.Parameter(torch.FloatTensor(feature_out,feature_in))
         nn.init.xavier_normal_(self.weights)
+
     def forward(self, features, targets):
         cos_theta = F.linear(features, F.normalize(self.weights), bias=None)
         cos_theta = cos_theta.clip(-1+1e-7, 1-1e-7)
