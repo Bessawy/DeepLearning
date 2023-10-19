4176dfcb8a4a626bccc6af3b6d4f1cfe6c9349e9
Amr Alkhashab <amr.alkhashab@innokasmedical.fi>
2023-10-19 12:52:03 +0300

    Add: cosine distance

--- test.py
+++ test.py
@@ -2,3 +2,11 @@ from models import Protonet
 from models.utils import Engine
 import torch
 
+from models.utils.dist import cosin_distance
+
+
+
+x = torch.tensor([[1.0, 2.0], [-1.0, -2.0]])
+y = torch.tensor([[1.0, 2.0]])
+
+print(cosin_distance(x, y))
